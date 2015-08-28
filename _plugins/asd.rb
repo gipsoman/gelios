37f8062a4ba028fa344b53aff329420e9b023991
droboshok <droboshok@yahoo.com>
2015-08-28 21:28:12 +0300

    flickr plugins2

--- _plugins/asd.rb
+++ _plugins/asd.rb
@@ -9,13 +9,12 @@ module Jekyll
         @ft   = markup.split(' ')[0]
         @lb   = markup.split("'")[1]
         html = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=84ad7df61b82e136a98bbf99fa997b3e&photo_id='+@ft
-        info = 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=84ad7df61b82e136a98bbf99fa997b3e&photo_id='+@ft
         @best = {
           :sizes => {}
         }
         page = Nokogiri::HTML(open(html))
         page.css("size[label='#{@lb}']").each do |el|
-         
+          puts el
           @best = {
             :width => el['width'],
             :height => el['height'],
@@ -23,23 +22,18 @@ module Jekyll
             :url => el['url'],
             :media => el['media']
           }
+         
 
+          
         end
-        doc = Nokogiri::HTML(open(info))
-        doc.css("photo").each do |link|
-          @best = {
-            :title => link['title']
-          }
-
-          puts @best
-        end
-
-        # подключаем Nokogiri
-        def render(context)
-        "<a href=\"#{@best[:source]}\" width=\"800\"><img src='#{@best[:source]}'><a/>"
-        end
-      
       end
+
+      # подключаем Nokogiri
+      def render(context)
+        "#{@lb}+#{@best}<br><img src='#{@best[:source]}'>"
+      end
+      
+
 
   end
 end
