70b9a0f19b20b84a665351aae3eb2bfbbcf85609
droboshok <droboshok@yahoo.com>
2015-08-28 21:11:25 +0300

    flickr plugins

---
+++ _plugins/asd.rb
@@ -0,0 +1,48 @@
+require 'open-uri'
+require 'nokogiri'
+module Jekyll
+  class MPTag < Liquid::Tag
+
+      def initialize(tag_name, markup, tokens)
+        super
+        @markup = markup
+        @ft   = markup.split(' ')[0]
+        @lb   = markup.split("'")[1]
+        html = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=84ad7df61b82e136a98bbf99fa997b3e&photo_id='+@ft
+        info = 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=84ad7df61b82e136a98bbf99fa997b3e&photo_id='+@ft
+        @best = {
+          :sizes => {}
+        }
+        page = Nokogiri::HTML(open(html))
+        page.css("size[label='#{@lb}']").each do |el|
+         
+          @best = {
+            :width => el['width'],
+            :height => el['height'],
+            :source => el['source'],
+            :url => el['url'],
+            :media => el['media']
+          }
+
+        end
+        doc = Nokogiri::HTML(open(info))
+        doc.css("photo").each do |link|
+          @best = {
+            :title => link['title']
+          }
+
+          puts @best
+        end
+
+        # подключаем Nokogiri
+        def render(context)
+        "<a href=\"#{@best[:source]}\" width=\"800\"><img src='#{@best[:source]}'><a/>"
+        end
+      
+      end
+
+  end
+end
+
+Liquid::Template.register_tag('m_p', Jekyll::MPTag)
+
